112f5d8d0f748d6a56b3cec682317bfc87af6a68
Daniil Chaniia <108178572+daniilchaniia@users.noreply.github.com>
2026-04-09 12:12:38 +0300

    Modify user_events table creation and CSV path
    Updated the user_events table creation script with comments in Russian for clarity. Changed the CSV file path from '/data/user_events.csv' to '/seed/user_events.csv'.

--- ecommerce/conversion_from_csv.sql
+++ ecommerce/conversion_from_csv.sql
@@ -1,12 +1,18 @@
-CREATE TABLE IF NOT EXISTS user_events (
-    event_id TEXT,
-    user_id TEXT,
-    event_type TEXT,
-    event_date TEXT,
-    product_id TEXT,
-    amount TEXT,
-    traffic_source TEXT
+
+--Создание изначальных таблиц для безопасного импортирования CSV данных.
+--Все таблицы создаются с текстовыми полями, чтобы избежать ошибок при загрузке.
+
+CREATE TABLE IF NOT EXISTS user_events ( --Внизу предоставлена логика содержащаяся в таблице до импортирования.
+    event_id TEXT, -- SERIAL PRIMARY KEY
+    user_id TEXT, -- INTEGER NOT NULL идентификатор пользователя, который совершил событие
+    event_type TEXT, --VARCHAR(50) NOT NULL тип события (например, "purchase", "view", "click")
+    event_date TEXT, --TIMESTAMP NOT NULL дата и время события
+    product_id TEXT, --INTEGER NOT NULL идентификатор продукта, связанного с событием
+    amount TEXT, --NUMERIC(12,2) сумма, связанная с событием (например, стоимость покупки)
+    traffic_source TEXT --VARCHAR(100) источник трафика, откуда пришел пользователь (например, "organic", "paid", "referral")
 );
+
+--Загрузка данных из CSV файлов в созданные таблицы.
 
 COPY user_events (
     event_id,
@@ -17,12 +23,14 @@ COPY user_events (
     amount,
     traffic_source
 )
-FROM '/data/user_events.csv'
+FROM '/seed/user_events.csv'
 WITH (
-    FORMAT csv,
-    HEADER true
+    FORMAT csv, --указываем, что файл в формате CSV
+    HEADER true --пропускаем первую строку с названиями столбцов
 );
 
+--После загрузки данных, необходимо изменить типы данных в таблице на соответствующие,
+--чтобы обеспечить корректную работу с данными и выполнение запросов.
 ALTER TABLE user_events
     ALTER COLUMN event_id TYPE INTEGER USING event_id::INTEGER,
     ALTER COLUMN user_id TYPE INTEGER USING user_id::INTEGER,
